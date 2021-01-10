081f6c243cbb16119c24b7d078cd298bf1d3a5d3
SangWoo Han <mgu1228@gmail.com>
2021-01-10 12:08:02 +0900

    Add PASS/FAIL folder making script in script.py

--- script.py
+++ script.py
@@ -1,3 +1,6 @@
+# 코딩 테스트 레포 일일 폴더 생성 스크립트
+# Windows 작업 스케줄러에 등록하거나 시작프로그램에 등록하여 사용
+
 import os
 import datetime
 
@@ -7,3 +10,8 @@ folder_name = today[0][2:] + today[1] + today[2]
 
 if not os.path.isdir("./{}".format(folder_name)):
     os.mkdir("./{}".format(folder_name))
+
+    os.mkdir("./{}/FAIL".format(folder_name))
+    os.mkdir("./{}/PASS".format(folder_name))
+
+exit()
